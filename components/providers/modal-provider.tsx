d3818422f8914fd76d9f44465216e2b7ca9423e4
Fufuos <thenicholasuser@yahoo.com>
2023-10-29 00:35:11 -0500

    created invitations functionality and semi-role

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 
 import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { InviteModal } from "@/components/modals/invite-modal";
 
 // Prevents modals from being rendered on the server side because that can create inconsistencies and therefore hydration errors
 
@@ -20,6 +21,7 @@ export const ModalProvider = () => {
   return (
     <>
       <CreateServerModal />
+      <InviteModal />
     </>
   )
 }
