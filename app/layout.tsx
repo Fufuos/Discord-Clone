f91e1d3d3c66f7d51fb1fd1791c8082b2dde63ea
Fufuos <thenicholasuser@yahoo.com>
2023-12-01 02:54:06 -0600

    created modular chat messaging component

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import { ModalProvider } from '@/components/providers/modal-provider'
 import { SocketProvider } from '@/components/providers/socket-provider'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ClerkProvider } from '@clerk/nextjs'
+import { QueryProvider } from '@/components/providers/query-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -37,7 +38,9 @@ export default function RootLayout({
           >
             <SocketProvider>
               <ModalProvider />
-              {children}
+              <QueryProvider>
+                {children}
+              </QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
